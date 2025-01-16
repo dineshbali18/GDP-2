17cd61dc5d7ab9d55c93acaef4723db2d74065a8
Kalyan Chakravarthy Jogula <jogulakalyan8@gmail.com>
2025-01-15 22:02:49 -0600

    usersroutes

--- code/BearcatFinance/index.js
+++ code/BearcatFinance/index.js
@@ -5,13 +5,13 @@ const port = 3000;
 const budgetRoutes = require('./routes/budgetRoutes');
 const savingGoalsRoutes = require('./routes/savingGoalsRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
-
+const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 
 app.use('/api/budget', budgetRoutes);
 app.use('/api/saving-goals', savingGoalsRoutes);
 app.use('/api/expenses', expenseRoutes);
-
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('Server is running...');
 });
